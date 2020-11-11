dd4e1de938be81eb4b43b2ebe0ec42584569fd54
Rodrigo <rodrigocorrea2@hotmail.com>
2020-11-11 17:54:06 -0600

    some functions

--- src/app/pages/auth/login/login.page.ts
+++ src/app/pages/auth/login/login.page.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ModalController, NavController } from '@ionic/angular';
+import { RegisterPage } from '../register/register.page';
+import { NgForm } from '@angular/forms';
+import { AuthService } from 'src/app/services/auth.service';
+import { AlertService } from 'src/app/services/alert.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +12,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  constructor(
+    private modalController: ModalController,
+    private authService: AuthService,
+    private navCtrl: NavController,
+    private alertService: AlertService
+  ) { }
 
   ngOnInit() {
   }
 
+  dismissLogin () {
+    this.modalController.dismiss();
+  }
+
+  async registerModal() {
+    this.dismissLogin();
+    const registerModal = await this.modalController.create({
+      component: RegisterPage
+    });
+    return await registerModal.present();
+  }
+
+  login(form: NgForm) {
+    this.authService.login(form.value.email, form.value.password).subscribe(
+      data => {
+        this.alertService.presentToast('Logged In');
+      },
+      error => {
+        console.log(error);
+      },
+      () => {
+        this.dismissLogin();
+        this.navCtrl.navigateRoot('/dashboard');
+      }
+    );
+  }
 }
